fb429a206c53eeed6d5e0f1ec220948d0be6191d
Junwei Lin <wertycxx@gmail.com>
2024-12-08 14:22:33 +0800

    setup app & some components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,48 @@
 <template>
+  <Navbar/>
   <router-view/>
+  <Footer/>
 </template>
 
 <script>
+import { computed, onMounted } from 'vue';
+import { useStore } from 'vuex';
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+
 export default {
   name: 'App',
+  components: {
+    Navbar,
+    Footer,
+  },
   setup() {
+    const store = useStore();
     
+    const isLoading = computed(() => store.getters.isUserLoading);
+    const hasUser = store.getters.hasUser;
+
+    // load user when app starts 
+    onMounted(() => {
+      if (isLoading.value) {
+        store.dispatch('fetchUser');
+      }
+    })
+
+    return { isLoading, hasUser };
   }
 }
 </script>
 
 <style>
-
+body {
+  margin: 0;
+  font-family: sans-serif;
+}
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100svh;
+  overflow: auto;
+}
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>navbar</header>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
+
+<style scoped>
+
+</style>
