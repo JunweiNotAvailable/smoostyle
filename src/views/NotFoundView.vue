b80600d5cf66d3c6e53d2ce291c8135d137e2cfd
Junwei Lin <wertycxx@gmail.com>
2024-12-16 15:18:59 +0800

    added 404

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    
+  <div class="not-found">
+    <div class="not-found-message">The page you are looking for does not exist.</div>
+    <button @click="$router.push({ name: 'Home' })" class="primary-button">Back to home</button>
   </div>
 </template>
 
@@ -11,5 +12,16 @@ export default {
 </script>
 
 <style scoped>
-
+.not-found {
+  width: 100vw;
+  height: 100svh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+.not-found-message {
+  font-weight: 300;
+  margin: 16px 0;
+}
 </style>
